e47a9dc2bf088d18a42c821de1c80211a8cb3282
Eric Mariasis <eric.mariasis@tufts.edu>
2013-05-05 14:40:05 -0300

    Update composerAppDelegate.h

--- composer/composer/composerAppDelegate.h
+++ composer/composer/composerAppDelegate.h
@@ -2,8 +2,8 @@
 //  composerAppDelegate.h
 //  composer
 //
-//  Created by Jay Wang on 3/10/13.
-//  Copyright (c) 2013 Jay Wang. All rights reserved.
+//  Created by Jay Wang, Eric Mariasis, and Scott Jacobson.
+//  Copyright (c) 2013 Jay Wang, Eric Mariasis, and Scott Jacobson. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>

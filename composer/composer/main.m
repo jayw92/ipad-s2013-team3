524c3632241cd0f33d6be11eeb7588fec96791b0
Eric Mariasis <eric.mariasis@tufts.edu>
2013-05-05 14:34:19 -0300

    Update main.m

--- composer/composer/main.m
+++ composer/composer/main.m
@@ -2,8 +2,8 @@
 //  main.m
 //  composer
 //
-//  Created by Jay Wang on 3/10/13.
-//  Copyright (c) 2013 Jay Wang. All rights reserved.
+//  Created by Jay Wang, Eric Mariasis, and Scott Jacobson.
+//  Copyright (c) 2013 Jay Wang, Eric Mariasis, Scott Jacobson. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
